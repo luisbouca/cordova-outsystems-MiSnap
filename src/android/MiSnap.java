bd63d29ec1c5c6359446454c07eafa65bbab5cca
Luis B <luismrbouca@gmail.com>
2022-08-01 09:06:09 +0100

    Fix: android permission

--- src/android/MiSnap.java
+++ src/android/MiSnap.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.Manifest;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -17,6 +18,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
@@ -96,11 +98,31 @@ public class MiSnap extends CordovaPlugin {
         if (action.equals("startFacialCapture")) {
             this.startFacialCaptureWorkflow(args);
             return true;
+        }else if (action.equals("checkPermission")) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    callback.sendPluginResult(new PluginResult(PluginResult.Status.OK,checkPermissionAction()));
+                }
+            });
+            return true;
+        } else if (action.equals("requestPermission")) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    try {
+                        requestPermissionAction();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        callbackContext.error("Request permission has been denied.");
+                        callback = null;
+                    }
+                }
+            });
+            return true;
         }
         callbackContext.error("Action not mapped!");
+        callback = null;
         return false;
     }
-
 
     private void startFacialCaptureWorkflow(JSONArray args) throws JSONException {
         // Prevent multiple MiSnap instances by preventing multiple button presses
@@ -238,6 +260,39 @@ public class MiSnap extends CordovaPlugin {
             case FacialCaptureWorkflowApiConstants.COUNTDOWN_TIMER_STYLE_NONE:
             default:
                 return 0;
+        }
+    }
+    
+    @Override
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        if (callback == null) {
+            return;
+        }
+
+        JSONObject returnObj = new JSONObject();
+        if (permissions != null && permissions.length > 0) {
+            //Call checkPermission again to verify
+            boolean hasPermission = checkPermissionAction();
+            callback.sendPluginResult(new PluginResult(PluginResult.Status.OK,hasPermission));
+        } else {
+            callback.error("Unknown error.");
+        }
+        callback = null;
+    }
+
+    private boolean checkPermissionAction() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        } else {
+            return cordova.hasPermission(Manifest.permission.CAMERA);
+        }
+    }
+
+    private void requestPermissionAction() throws Exception {
+        if (checkPermissionAction()) {
+            callback.sendPluginResult(new PluginResult(PluginResult.Status.OK,true));
+        } else {
+            cordova.requestPermissions(this, 10001, new String[]{Manifest.permission.CAMERA});
         }
     }
 }
--- www/MiSnap.js
+++ www/MiSnap.js
@@ -3,3 +3,10 @@ var exec = require('cordova/exec');
 exports.startFacialCapture = function (configs, success, error) {
     exec(success, error, 'MiSnap', 'startFacialCapture', [configs]);
 };
+
+exports.requestPermission = function (success, error) {
+    exec(success, error, 'MiSnap', 'requestPermission', []);
+};
+exports.checkPermission = function (success, error) {
+    exec(success, error, 'MiSnap', 'checkPermission', []);
+};
